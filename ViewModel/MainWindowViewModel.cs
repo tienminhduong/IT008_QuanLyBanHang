73cdfd61c355330695ab10bcb415e01ca947dd75
TienMinhDuong <tmd31052005@gmail.com>
2024-11-12 20:29:34 +0700

    Install MaterialDesignThemes and CommunityToolkit for MVVM

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using IT008_QuanLyBanHang.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,12 @@ using System.Windows;
 
 namespace IT008_QuanLyBanHang.ViewModel
 {
-    public partial class TongQuanViewModel : ObservableObject
+    public partial class MainWindowViewModel : ObservableObject
     {
+        [RelayCommand]
+        void Hello()
+        {
+            MessageBox.Show("Hello world!");
+        }
     }
 }
