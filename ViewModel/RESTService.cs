86ad3a2ae8d1dfb74c9f489980a8a722da13922f
TienMinhDuong <tmd31052005@gmail.com>
2024-11-19 16:22:15 +0700

    Verify password for login

--- ViewModel/RESTService.cs
+++ ViewModel/RESTService.cs
@@ -4,7 +4,10 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Security.Principal;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -23,10 +26,34 @@ namespace IT008_QuanLyBanHang.ViewModel
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                Trace.WriteLine(ex.Message);
             }
             return string.Empty;
-            //return await client.SendAsync(request).Result.Content.ReadAsStringAsync();
+        }
+
+        public async Task<bool> TryLogin(string account, string password)
+        {
+            string jsonContent = $"{{\"account\":\"{account}\",\"password\":\"{password}\"}}";
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, "users/sign_in");
+                request.Content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var respond = await client.SendAsync(request);
+                respond.EnsureSuccessStatusCode();
+                AccessToken? accessToken = await respond.Content.ReadFromJsonAsync<AccessToken>();
+                string? token = accessToken?.data.Token;
+                if (!hasLogin)
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+                    hasLogin = true;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);
+                return false;
+            }
         }
 
         #region Singleton
@@ -44,10 +71,19 @@ namespace IT008_QuanLyBanHang.ViewModel
         private RESTService()
         {
             client.BaseAddress = new Uri("https://store-manager-server-8dd2e1e19873.herokuapp.com/api/v1/");
-            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {API.API_TOKEN}");
         }
 
 
         HttpClient client = new();
+        bool hasLogin = false;
+    }
+    class AccessToken
+    {
+        public AccessTokenData data { get; set; } = new AccessTokenData();
+    }
+    class AccessTokenData
+    {
+        [JsonPropertyName("access_token")]
+        public string Token { get; set; } = "";
     }
 }
