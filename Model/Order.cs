b7a566db0f48d53c6802af773bd0d9385660e854
TienMinhDuong <tmd31052005@gmail.com>
2024-12-11 22:52:21 +0700

    Add Customer model

---
+++ Model/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT008_QuanLyBanHang.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public float TotalAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string? Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Customer? Customer { get; set; }
+
+    }
+    public class OrderItem
+    {
+
+    }
+}
