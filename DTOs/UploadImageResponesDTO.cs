3dfca9ae6205eb5f54f92a3baacc43a4dbd89282
TienMinhDuong <tmd31052005@gmail.com>
2024-12-16 05:43:31 +0700

    Done

---
+++ DTOs/UploadImageResponesDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT008_QuanLyBanHang.DTOs
+{
+    public class UploadImageResponesDTO
+    {
+        public string signature { get; set; }
+        public string api_key { get; set; }
+        public ulong timestamp { get; set; }
+        public string public_id { get; set; }
+        public string cloud_name { get; set; }
+    }
+}
